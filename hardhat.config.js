0bc9d70506f9a6a5b8f7c75397e135e539c335d4
Javier Alaves <jayalaves@gmail.com>
2025-06-11 23:28:02 +0200

    Initial bundle prototype

---
+++ hardhat.config.js
@@ -0,0 +1,15 @@
+require("@nomicfoundation/hardhat-toolbox");
+const path = require("path");
+
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.24",
+        settings: {},
+        // Use local solcjs shipped with the solc npm package
+        path: require.resolve("solc")
+      }
+    ],
+  },
+};
